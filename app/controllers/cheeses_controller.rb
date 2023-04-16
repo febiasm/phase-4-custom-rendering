762629fecbc2561ba1abd4bc86d2b98fe1d28992
febiasm <trevorfebias@gmail>
2023-04-16 21:12:34 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -9,7 +9,12 @@ class CheesesController < ApplicationController
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
+    if cheese 
+    cheese = Cheese.find_by(id: params[:id])
     render json: cheese
+    else 
+      render json:{error: '404 not found'}, status: 404
+    end
   end
 
 end
